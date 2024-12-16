d61b32506d51f15ea7b2d774df93b808de64dd1e
Arda Korkmaz <109353825+korkmaz-arda@users.noreply.github.com>
2024-12-16 14:47:04 +0300

    Cosmetic changes

--- wipe.py
+++ wipe.py
@@ -4,14 +4,13 @@ import os
 SUPPORTED_TYPES = ['.jpg', '.jpeg']
 
 
-def remove_metadata(file_path):
+def strip_metadata(file_path):
     with Image.open(file_path) as img:
-        img_format = img.format
-        img.save(file_path, format=img_format)
+        img.save(file_path, format=img.format)
 
 
 def clean_dir(dir):
     for root, _, files in os.walk(dir):
         for file in files:
             if os.path.splitext(file)[1].lower() in SUPPORTED_TYPES:
-                remove_metadata(os.path.join(root, file))
+                strip_metadata(os.path.join(root, file))
