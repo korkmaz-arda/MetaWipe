38d917e2abd429f87f51fd21be6b4bf0e0a17a13
korkmaz-arda <arda.korkmaz1996@gmail.com>
2025-01-27 09:42:55 +0300

    Add support for WebP files

--- wipe.py
+++ wipe.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-SUPPORTED_TYPES = ['.jpg', '.jpeg', '.png', '.svg', '.gif', '.bmp', '.tiff']
+SUPPORTED_TYPES = ['.jpg', '.jpeg', '.png', '.svg', '.gif', '.bmp', '.tiff', '.webp']
 
 
 def strip_metadata(file_path, output_path=None, verbose=True):
